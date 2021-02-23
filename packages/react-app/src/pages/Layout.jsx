298229c0dd75acb8e6133533d7f94d2d590a809e
Cristopher Ortega <ortega.cpp@gmail.com>
2021-02-23 18:13:41 -0400

    Fix providerChainId type

--- packages/react-app/src/pages/Layout.jsx
+++ packages/react-app/src/pages/Layout.jsx
@@ -7,7 +7,10 @@ import { Header, ConnectWeb3 } from 'components/index';
 export const Layout = ({ children }) => {
   const { account, providerChainId } = useContext(Web3Context);
   const valid =
-    !!account && (providerChainId === '100' || providerChainId === '137');
+    !!account && (providerChainId === 100 || providerChainId === 137);
+
+  // eslint-disable-next-line no-console
+  console.log(valid, account, providerChainId);
 
   return (
     <div>
