3c2c1a4fe5685467ad435ad3a478651947b6cb0c
Rahul Sethuram <rahul@sethuram.com>
2021-04-07 19:57:05 +0200

    Add ability to change assets

--- packages/react-app/src/components/Modal.jsx
+++ packages/react-app/src/components/Modal.jsx
@@ -5,12 +5,57 @@ import { ConnextModal } from '@connext/vector-modal';
 import { Grid, Button, TextField, Select, MenuItem } from '@material-ui/core';
 import getRpcUrl from 'lib/rpc';
 
-const Modal = (props) => {
+const NETWORKS = [
+  {
+    assetId: '0x0000000000000000000000000000000000000000',
+    chainName: 'xDai Chain',
+    chainId: 100,
+    assets: {
+      DAI: '0x0000000000000000000000000000000000000000',
+      USDC: '0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83',
+      USDT: '0x4ECaBa5870353805a9F068101A40E0f32ed605C6',
+    },
+  },
+  {
+    assetId: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
+    chainName: 'Matic Mainnet',
+    chainId: 137,
+    assets: {
+      DAI: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
+      USDC: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+      USDT: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
+    },
+  },
+  {
+    assetId: '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3',
+    chainName: 'Binance Smart Chain Mainnet',
+    chainId: 56,
+    assets: {
+      DAI: '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3',
+      USDC: '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d',
+      USDT: '0x55d398326f99059fF775485246999027B3197955',
+    },
+  },
+  // {
+  //   assetId: '0x0298c2b32eaE4da002a15f36fdf7615BEa3DA047',
+  //   chainName: 'Huobi ECO Chain Mainnet',
+  //   chainId: 128,
+  //   assetName: 'HUSD',
+  // },
+];
+
+const ASSETS = ['DAI', 'USDC', 'USDT'];
+
+const Modal = () => {
   const { web3Provider, account } = useContext(Web3Context);
   const [showModal, setShowModal] = useState(false);
   const [withdrawalAddress, setWithdrawalAddress] = useState(account);
   const [senderOpen, setSenderOpen] = useState(false);
   const [receiverOpen, setReceiverOpen] = useState(false);
+  const [assetOpen, setAssetOpen] = useState(false);
+  const [asset, setAsset] = useState(ASSETS[0]);
+  const [senderChain, setSenderChain] = useState(NETWORKS[0]);
+  const [receiverChain, setReceiverChain] = useState(NETWORKS[1]);
 
   const handleChange = (event) => {
     setWithdrawalAddress(event.target.value);
@@ -24,35 +69,6 @@ const Modal = (props) => {
     }
     return errors;
   };
-
-  const NETWORKS = [
-    {
-      assetId: '0x0000000000000000000000000000000000000000',
-      chainName: 'xDai Chain',
-      chainId: 100,
-      assetName: 'XDAI',
-    },
-    {
-      assetId: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
-      chainName: 'Matic Mainnet',
-      chainId: 137,
-      assetName: 'DAI',
-    },
-    {
-      assetId: '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3',
-      chainName: 'Binance Smart Chain Mainnet',
-      chainId: 56,
-      assetName: 'DAI',
-    },
-    // {
-    //   assetId: '0x0298c2b32eaE4da002a15f36fdf7615BEa3DA047',
-    //   chainName: 'Huobi ECO Chain Mainnet',
-    //   chainId: 128,
-    //   assetName: 'HUSD',
-    // },
-  ];
-  const [senderChain, setSenderChain] = useState(NETWORKS[0]);
-  const [receiverChain, setReceiverChain] = useState(NETWORKS[1]);
 
   const swapChains = () => {
     const s = senderChain;
@@ -102,7 +118,11 @@ const Modal = (props) => {
               justify="center"
               size="small"
             >
-              <Button variant="outlined" style={{ border: 'none' }} onClick={swapChains}>
+              <Button
+                variant="outlined"
+                style={{ border: 'none' }}
+                onClick={swapChains}
+              >
                 {'<>'}
               </Button>
             </Grid>
@@ -132,6 +152,35 @@ const Modal = (props) => {
                     disabled={senderChain.chainId === t.chainId}
                   >
                     {t.chainName} - {t.assetName}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          spacing={2}
+          alignItems="center"
+          justify="center"
+          alignContent="center"
+        >
+          <Grid item xs={4}>
+            <Select
+              id="asset"
+              open={assetOpen}
+              onClose={() => setAssetOpen(false)}
+              onOpen={() => setAssetOpen(true)}
+              onChange={(event) => setAsset(ASSETS[event.target.value])}
+              fullWidth
+              defaultValue={1}
+              value={ASSETS.findIndex((a) => a === asset)}
+              // component={Select}
+            >
+              {ASSETS.map((t, index) => {
+                return (
+                  <MenuItem value={index} key={index}>
+                    {t}
                   </MenuItem>
                 );
               })}
@@ -183,9 +232,9 @@ const Modal = (props) => {
         <ConnextModal
           showModal={showModal}
           routerPublicIdentifier="vector892GMZ3CuUkpyW8eeXfW2bt5W73TWEXtgV71nphXUXAmpncnj8"
-          depositAssetId={senderChain.assetId}
+          depositAssetId={senderChain.assets[asset]}
           depositChainId={senderChain.chainId}
-          withdrawAssetId={receiverChain.assetId}
+          withdrawAssetId={receiverChain.assets[asset]}
           withdrawChainId={receiverChain.chainId}
           withdrawalAddress={withdrawalAddress}
           onClose={() => setShowModal(false)}
