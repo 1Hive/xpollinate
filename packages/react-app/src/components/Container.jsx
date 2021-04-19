7b49a8b6cdf63a2fb6da5da80985774fece5cddd
Rahul Sethuram <rahul@sethuram.com>
2021-04-19 19:01:38 +0400

    Add less confusing wording

--- packages/react-app/src/components/Container.jsx
+++ packages/react-app/src/components/Container.jsx
@@ -41,6 +41,20 @@ const Container = () => {
         borderWidth="1px"
         borderRadius="lg"
       >
+        Use the below table to determine if there is liquidity to support your
+        desired swap. The amount in the table should be larger than the amount
+        and chain you are trying to swap TO (not from).
+      </Box>
+      <Box
+        width={{ sm: 'full', md: 'auto' }}
+        w={[300, 400, 560]}
+        alignItems="center"
+        padding="1rem"
+        margin="0.5rem"
+        flexGrow={1}
+        borderWidth="1px"
+        borderRadius="lg"
+      >
         <AvailableLiquidity />
       </Box>
     </Flex>
