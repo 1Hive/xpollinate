dd49c4a0e231efd375b0e5fe4f150d574a8d2dbc
Cristopher Ortega <ortega.cpp@gmail.com>
2021-04-21 23:19:40 -0400

    Add text over the bridge component

--- packages/react-app/src/pages/Home.jsx
+++ packages/react-app/src/pages/Home.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Text } from '@chakra-ui/react';
 import { Container } from 'components/index';
 
-export const Home = (props) => {
-  return <Container />;
-};
+export const Home = (props) => (
+  <div>
+    <Text align="center" p="1rem" fontWeight="bold" fontSize="2xl">
+      Cross-Chain Bridge
+    </Text>
+    <Container />
+  </div>
+);
