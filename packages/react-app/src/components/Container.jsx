1c8d0187bb724643a603b24230119fef707b6a83
Cristopher Ortega <ortega.cpp@gmail.com>
2021-04-22 01:33:13 -0400

    Improving styles on container

--- packages/react-app/src/components/Container.jsx
+++ packages/react-app/src/components/Container.jsx
@@ -9,24 +9,21 @@ const Container = () => {
         width={{ sm: 'full', md: 'auto' }}
         w={[300, 400, 560]}
         alignItems="center"
-        padding="1rem"
+        padding="2.5rem"
         margin="0.5rem"
-        flexGrow={1}
-        borderWidth="1px"
-        borderRadius="lg"
+        bgColor="white"
+        boxShadow="0px 1rem 2rem rgba(204, 218, 238, 0.8)"
+        borderRadius="1rem"
       >
         <Modal />
       </Box>
       <Box
-        width={{ sm: 'full', md: 'auto' }}
-        w={[300, 400, 560]}
-        alignItems="center"
+        bgGradient="linear-gradient(257.5deg, rgba(235, 0, 85, 0.3) -39.73%, rgba(255, 250, 128, 0.3) 107.97%)"
+        w={[250, 350, 500]}
         padding="1rem"
-        margin="0.5rem"
-        flexGrow={1}
-        borderWidth="1px"
+        margin="2rem"
         borderRadius="lg"
-        bg="red.100"
+        fontWeight="light"
       >
         This is beta software, use at your own risk.
       </Box>
