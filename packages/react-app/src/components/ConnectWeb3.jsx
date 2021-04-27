87ce62ca668afb26d96503bfe906c2e26647e29f
Cristopher Ortega <ortega.cpp@gmail.com>
2021-04-27 16:04:32 -0400

    Centering the Web3Connect component

--- packages/react-app/src/components/ConnectWeb3.jsx
+++ packages/react-app/src/components/ConnectWeb3.jsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Text, Image } from '@chakra-ui/react';
+import { Button, Flex, Text, Image, Center } from '@chakra-ui/react';
 import React, { useContext } from 'react';
 
 import { Web3Context } from 'contexts/Web3Context';
@@ -14,46 +14,46 @@ const ConnectWeb3 = () => {
   const { connectWeb3, loading, account, disconnect } = useContext(Web3Context);
 
   return (
-    <Flex
-      bgColor="white"
-      bgGradient={styles['light-gradient']}
-      boxShadow="0px 1rem 2rem rgba(204, 218, 238, 0.8)"
-      borderRadius="1rem"
-      direction="column"
-      align="center"
-      w="calc(100% - 2rem)"
-      mt="5rem"
-      p="2rem"
-      maxW="28.5rem"
-      mx={4}
-    >
-      <Image
-        src="https://storage.googleapis.com/1hive/xpollinate/logo.svg"
-        alt="xPollinate Logo"
-      ></Image>
-      <Text fontWeight="light" m="1rem">
-        Please, connect your wallet.
-      </Text>
-      {account && !loading ? (
-        <Button onClick={disconnect} colorScheme="orange" px={12}>
-          Disconnect
-        </Button>
-      ) : (
-        <Button
-          onClick={connectWeb3}
-          fontWeight="light"
-          _focus={{ boxShadow: 'outline' }}
-          _hover={{
-            bg: styles['bold-gradient'],
-          }}
-          bgGradient={styles['bold-gradient']}
-          maxW="15rem"
-          isLoading={loading}
-        >
-          Connect
-        </Button>
-      )}
-    </Flex>
+    <Center>
+      <Flex
+        bgColor="white"
+        bgGradient={styles['light-gradient']}
+        boxShadow="0px 1rem 2rem rgba(204, 218, 238, 0.8)"
+        borderRadius="1rem"
+        direction="column"
+        align="center"
+        w="calc(100% - 2rem)"
+        p="2rem"
+        minW="15rem"
+      >
+        <Image
+          src="https://storage.googleapis.com/1hive/xpollinate/logo.svg"
+          alt="xPollinate Logo"
+        ></Image>
+        <Text fontWeight="light" m="1rem">
+          Please, connect your wallet.
+        </Text>
+        {account && !loading ? (
+          <Button onClick={disconnect} colorScheme="orange" px={12}>
+            Disconnect
+          </Button>
+        ) : (
+          <Button
+            onClick={connectWeb3}
+            fontWeight="light"
+            _focus={{ boxShadow: 'outline' }}
+            _hover={{
+              bg: styles['bold-gradient'],
+            }}
+            bgGradient={styles['bold-gradient']}
+            maxW="15rem"
+            isLoading={loading}
+          >
+            Connect
+          </Button>
+        )}
+      </Flex>
+    </Center>
   );
 };
 
