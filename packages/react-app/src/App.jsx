cbf04d2e75bbddd40e398c857381e40b3313aa41
Cristopher Ortega <ortega.cpp@gmail.com>
2021-04-30 01:14:06 -0400

    Fix linting erros

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -1,11 +1,12 @@
-import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+// import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import { Web3Provider } from 'contexts/Web3Context';
 import { Routes } from './Routes';
 import { Layout } from './pages/Layout';
-import { theme } from './theme';
+// import { theme } from './theme';
 
 export const App = () => {
   return (
