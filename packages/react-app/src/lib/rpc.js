7423d12c38cb60586668da3f52731bc7799c29f8
Rahul Sethuram <rahul@sethuram.com>
2021-05-10 14:38:41 +0400

    Change RPCs

--- packages/react-app/src/lib/rpc.js
+++ packages/react-app/src/lib/rpc.js
@@ -3,11 +3,11 @@ export default function getRpcUrl(networkId) {
     case 56:
       return 'https://bsc-dataseed1.binance.org';
     case 100:
-      return 'https://rpc.xdaichain.com';
+      return 'https://xdai.poanetwork.dev';
     case 128:
       return 'https://http-mainnet.hecochain.com';
     case 137:
-      return 'https://rpc-mainnet.matic.network';
+      return 'https://matic-mainnet.chainstacklabs.com';
     default:
       throw new Error('No RPC configured for network: ', networkId);
   }
