46d2854aa3c058f64544f6197238b960798fe812
Rahul Sethuram <rahul@sethuram.com>
2021-05-17 08:10:05 -0700

    Public

--- packages/react-app/src/lib/rpc.js
+++ packages/react-app/src/lib/rpc.js
@@ -1,7 +1,7 @@
 export default function getRpcUrl(networkId) {
   switch (networkId) {
     case 56:
-      return 'https://dry-old-paper.bsc.quiknode.pro/aaefec831b81c9054475932c0b3330f3a3df4f71/';
+      return 'https://bsc-dataseed1.ninicoin.io/';
     case 100:
       return 'https://xdai.poanetwork.dev';
     case 128:
