7e310c771c99997a5f362319edc80076001bb84d
Rahul Sethuram <rahul@sethuram.com>
2021-05-20 11:47:49 -0700

    FTM

--- packages/react-app/src/lib/rpc.js
+++ packages/react-app/src/lib/rpc.js
@@ -3,11 +3,13 @@ export default function getRpcUrl(networkId) {
     case 56:
       return 'https://bsc-dataseed1.ninicoin.io/';
     case 100:
-      return 'https://xdai.poanetwork.dev';
+      return 'https://xdai.poanetwork.dev/';
     case 128:
-      return 'https://http-mainnet.hecochain.com';
+      return 'https://http-mainnet.hecochain.com/';
     case 137:
-      return 'https://ancient-lively-sky.matic.quiknode.pro/05cf5df0b0658d5cb3ab576ec4aefefc3731e0c5/';
+      return 'https://matic-mainnet.chainstacklabs.com/';
+    case 250:
+      return 'https://rpcapi.fantom.network/';
     default:
       throw new Error('No RPC configured for network: ', networkId);
   }
