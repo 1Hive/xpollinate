30c4b67414c0ba58a23a2ef09d9d0e7caa00fe08
Sanchay Mittal <sanchaymittal@gmail.com>
2021-05-26 20:56:05 +0530

    hotfix: matic rpc

--- packages/react-app/src/lib/rpc.js
+++ packages/react-app/src/lib/rpc.js
@@ -7,7 +7,7 @@ export default function getRpcUrl(networkId) {
     case 128:
       return 'https://http-mainnet.hecochain.com/';
     case 137:
-      return 'https://rpc-mainnet.maticvigil.com';
+      return 'https://matic-mainnet.chainstacklabs.com';
     case 250:
       return 'https://rpcapi.fantom.network/';
     default:
