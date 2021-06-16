dd2a55c24b1776dc58c9ce80ad82117f55704a08
LayneHaber <layne.haber@gmail.com>
2021-06-16 09:05:12 -0600

    Rotate rpcs

--- packages/react-app/src/lib/rpc.js
+++ packages/react-app/src/lib/rpc.js
@@ -1,13 +1,13 @@
 export default function getRpcUrl(networkId) {
   switch (networkId) {
     case 56:
-      return 'https://bsc-dataseed1.ninicoin.io/';
+      return 'https://bsc-dataseed2.defibit.io/';
     case 100:
       return 'https://xdai.poanetwork.dev/';
     case 128:
       return 'https://http-mainnet.hecochain.com/';
     case 137:
-      return 'https://rpc-mainnet.matic.quiknode.pro';
+      return 'https://matic-mainnet.chainstacklabs.com';
     case 250:
       return 'https://rpcapi.fantom.network/';
     default:
