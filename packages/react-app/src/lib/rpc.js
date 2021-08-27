fd2594a24f79700d2cdfafa5f612a3f0e60f7d22
Rahul Sethuram <rahul@sethuram.com>
2021-08-27 18:26:22 +0400

    RPC update

--- packages/react-app/src/lib/rpc.js
+++ packages/react-app/src/lib/rpc.js
@@ -7,7 +7,7 @@ export default function getRpcUrl(networkId) {
     case 128:
       return 'https://http-mainnet.hecochain.com/';
     case 137:
-      return 'https://rpc.polycat.finance/';
+      return 'https://matic-mainnet.chainstacklabs.com/';
     case 250:
       return 'https://rpcapi.fantom.network/';
     default:
