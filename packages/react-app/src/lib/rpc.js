b68268d280218530c7b1be73ed62a46a63a3ce5a
LayneHaber <layne.haber@gmail.com>
2021-08-28 17:40:52 -0600

    Test reliable rpc

--- packages/react-app/src/lib/rpc.js
+++ packages/react-app/src/lib/rpc.js
@@ -7,7 +7,7 @@ export default function getRpcUrl(networkId) {
     case 128:
       return 'https://http-mainnet.hecochain.com/';
     case 137:
-      return 'https://matic-mainnet.chainstacklabs.com/';
+      return 'https://polygon-mainnet.infura.io/v3/e02a34c8aa5d4156aeed1142ea2173c8';
     case 250:
       return 'https://rpcapi.fantom.network/';
     default:
